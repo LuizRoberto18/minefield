2bf253b47b54941d444a52a61802e7b7646e0b32
Luiz Roberto <luiz.santos@carajas.net.br>
2023-01-10 17:56:41 -0300

    crindo componente field e testando na tela minefield_app

---
+++ lib/components/field_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../components/field_widget.dart';
+import '../components/result_widget.dart';
+import '../models/field.dart';
+
+class FieldWidget extends StatelessWidget {
+  final Field field;
+  final void Function(Field) onOpen;
+  final void Function(Field) onChangeMarking;
+
+  const FieldWidget({
+    Key? key,
+    required this.field,
+    required this.onOpen,
+    required this.onChangeMarking,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onOpen(field),
+      onLongPress: () => onChangeMarking(field),
+      child: _getImage(),
+    );
+  }
+
+  Widget _getImage() {
+    int qtdeMinas = field.numberOfMinesInTheNeighborhood;
+    if(field.open && field.undermined && field.exploded){
+      return Image.asset("assets/images/bomba_0.jpeg");
+    }else if(field.open && field.undermined ){
+      return Image.asset("assets/images/bomba_1.jpeg");
+    }else if(field.open && qtdeMinas > 0 ){
+      return Image.asset("assets/images/aberto_$qtdeMinas.jpeg");
+    }else if(field.open){
+      return Image.asset("assets/images/bomba_0.jpeg");
+    }else if(field.undermined){
+      return Image.asset("assets/images/bandeira.jpeg");
+    }
+    else{
+      return Image.asset("assets/images/fechado.jpeg");
+    }
+  }
+}
--- lib/screens/minefield_app.dart
+++ lib/screens/minefield_app.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:minefield/components/field_widget.dart';
+import 'package:minefield/models/explosion_exception.dart';
+import 'package:minefield/models/field.dart';
 import '../components/result_widget.dart';
 
 class MineFieldApp extends StatelessWidget {
   const MineFieldApp({Key? key}) : super(key: key);
 
-  _restart() {
-    print("reiniciar");
-  }
-
   @override
   Widget build(BuildContext context) {
+    Field neighbor1 = Field(line: 0, column: 0);
+    neighbor1.undermine();
+    Field neighbor2 = Field(line: 0, column: 0);
+    neighbor2.undermine();
+    Field field = Field(line: 0, column: 0);
+    field.addNeighbor(neighbor1);
+    field.addNeighbor(neighbor2);
+
+    try {
+     // field.undermine();
+      field.changeMarking();
+    } on ExplosionException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultWidget(win: true, onRestart: _restart),
         body: Container(
-          child: Text("Tabuleiro"),
+          child: FieldWidget(field: field, onOpen: _open, onChangeMarking: _changeMarking),
         ),
       ),
     );
+  }
+
+  void _restart() {
+    print("reiniciar");
+  }
+
+  void _open(Field field) {
+    print("abrir");
+  }
+
+  void _changeMarking(Field field) {
+    print("Alternar marcação");
   }
 }
